9e270e9af856b2a5da8bf42241b912950783d9e7
bvelasqo <brandon.velasquez.osorio@gmail.com>
2024-03-12 14:18:10 -0500

    lang by country user

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,58 @@
-import createMiddleware from 'next-intl/middleware';
+import createIntlMiddleware from 'next-intl/middleware';
+import { headers } from 'next/headers';
+import { NextRequest } from 'next/server';
 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['en', 'es', 'fr'],
+export default async function middleware(request: NextRequest) {
+  const header = headers()
+  let ip = header.get('x-real-ip') as string;
 
-  // Used when no locale matches
-  defaultLocale: 'en'
-});
+  const forwardedFor = header.get('x-forwarded-for') as string
+  if (!ip && forwardedFor) {
+    ip = forwardedFor?.split(",").at(0) ?? "Unknown";
+  }
+  const lang = await getCountryByIp(ip);
+  // Step 2: Create and call the next-intl middleware (example)
+  const handleI18nRouting = createIntlMiddleware({
+    // A list of all locales that are supported
+    locales: ['en', 'es', 'fr'],
+    defaultLocale: getLangByCountryCode(lang),
+    // Used when no locale matches
+    localeDetection: false,
+    localePrefix: 'always',
+    pathnames: {
+      // The key is the pathname for the given locale
+      // The value is the pathname for the default locale
+      en: '/en',
+      es: '/es',
+      fr: '/fr'
+    }
+  });
+  const response = handleI18nRouting(request);
+
+  return response;
+}
+
+
 
 export const config = {
   // Match only internationalized pathnames
   matcher: ['/', '/(es|en|fr)/:path*']
 };
+
+async function getCountryByIp(ip: string) {
+  const response = await fetch(`http://ip-api.com/json/${ip}`);
+  const data = await response.json();
+  console.log('data', data);
+  return data.countryCode?.toLowerCase() ?? 'en';
+}
+
+function getLangByCountryCode(countryCode: string) {
+  switch (countryCode) {
+    case 'es' || 'mx' || 'ar' || 'co' || 'pe' || 've' || 'cl' || 'ec' || 'gt' || 'cu' || 'bo' || 'do' || 'hn' || 'py' || 'sv' || 'ni' || 'cr' || 'uy' || 'pa' || 'pr':
+      return 'es';
+    case 'fr' || 'ca' || 'be' || 'ch' || 'lu':
+      return 'fr';
+    default:
+      return 'en';
+  }
+}
